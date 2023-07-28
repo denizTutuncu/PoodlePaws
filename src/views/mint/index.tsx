e75d717c34bf93459391de967d4c03b4ea500a2b
Deniz Tutuncu <denizsafaktutuncu@gmail.com>
2023-07-27 23:57:35 -0700

    Remove Fetch candy machine

--- src/views/mint/index.tsx
+++ src/views/mint/index.tsx
@@ -6,7 +6,6 @@ import Link from "next/link"
 import { useWallet, useConnection } from "@solana/wallet-adapter-react"
 
 // Components
-import { FetchCandyMachine } from "../../components/FetchCandyMachine"
 import Mint from "../../components/Mint"
 
 import pkg from "../../../package.json"
@@ -47,19 +46,6 @@ export const MintView: FC = ({}) => {
 
         <img className={styles.product_image}src='/poodlepawsworldtour.png' alt='The Lira DAO'/>
         
-        {/* <div className="max-auto max-auto mockup-code bg-primary p-6 my-2">
-          <pre data-prefix="Wallet">
-          {wallet && wallet.connected && (
-            <p className="text-center text-3xl md:pl-12 font-bold text-transparent bg-clip-text bg-gradient-to-tr from-[#9945FF] to-[#14F195]">
-              {wallet.publicKey ? wallet.publicKey.toLocaleString() : ""}
-            </p>
-          )}
-          <pre data-prefix="Balance"></pre>
-            {wallet && wallet.connected && <p className="text-left text-4xl md:pl-12 font-bold text-transparent bg-clip-text bg-gradient-to-tr from-[#9945FF] to-[#14F195]">{(balance).toLocaleString()} SOL</p>}
-
-          </pre>
-        </div> */}
-
         <div className="text-center p-10">
           <a
             href="https://studio.metaplex.com/project/poodle-paws-world-tour"
@@ -69,21 +55,6 @@ export const MintView: FC = ({}) => {
           >
             Click to mint Poodle Paws World Tour on Metaflex
           </a>
-{/* 
-        <div className="text-center">
-            { wallet.connected ? <Mint/> : 
-              <div>
-                <p  className="text-center text-3xl md:pl-12 font-bold text-transparent bg-clip-text bg-gradient-to-tr from-[#9945FF] to-[#14F195]">
-                  Connect wallet to mint
-                </p>
-            </div>
-          }
-          <div className="text-center">
-            <FetchCandyMachine />
-          </div>
-      
-        </div> */}
-
         </div>
       </div>
     </div>
